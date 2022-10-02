47408aaeb6f8817cf76a3416404193b9c1a71bde
GreatSea1 <user.name>
2022-10-02 14:38:19 +0700

    ааааааа
    Попоже

---
+++ Progr_Lab_1/dichotomy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progr_Lab_1
+{
+  public partial class dichotomy : Form
+  {
+    public dichotomy()
+    {
+      InitializeComponent();
+    }
+
+    private void panel1_Paint(object sender, PaintEventArgs e)
+    {
+
+    }
+  }
+}
